e820b8ba199d6b8bdf52f2a6c9512c076723f529
gcollins1317 <gracecollins1000@gmail.com>
2018-07-10 18:42:26 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(x=5)
   if x%3==0 && x%5==0
-    "FizzBuzz"
+    puts "FizzBuzz"
     elsif x%3==0
-    "Fizz"
+    puts "Fizz"
     elsif x%5==0 
-    "Buzz"
+    puts "Buzz"
   else 
     return nil
   end 
