66867b48f03a24941d90bcdb97b27f2f4ec108f1
gcollins1317 <gracecollins1000@gmail.com>
2018-07-10 18:47:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,16 @@
 def fizzbuzz(x=gets.chomp)
   if x%3==0 && x%5==0
     "FizzBuzz"
+  end
     elsif x%3==0
     "Fizz"
+  end
     elsif x%5==0 
     "Buzz"
+  end 
   else 
-    end 
+    end
+  end 
   puts fizzbuzz 
   
     
